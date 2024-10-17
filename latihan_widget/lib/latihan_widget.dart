d225b4f9c41d519abb90e1d3d925d2158995b908
Jeree31 <jeremyallegratohartono_2327250004@mhs.mdp.ac.id>
2024-10-17 14:02:08 +0700

    inisialisasi wisata candi

---
+++ latihan_widget/lib/latihan_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: Text("Widget Demo"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            width: double.infinity,
+            child: Center(child: Text("Jeremy Allegrato Hartono", style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,)),
+          ),
+          ElevatedButton(
+            onPressed: (){},
+            child: const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text("Ini adalah Tombol Elevated"),
+              ),
+            ),
+            SizedBox(height: 16),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.yellow,),
+                Text("Rating 4.5")             
+                ],
+            ),
+            Image.network("https://picsum.photos/id/237/200/300", width: double.infinity, height: 200 ,)
+          ],
+        
+     ),
+);
+}
+}
---
+++ latihan_widget/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_widget/latihan_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+    home: WidgetDemo(),
+    );
+
+  }
+}
