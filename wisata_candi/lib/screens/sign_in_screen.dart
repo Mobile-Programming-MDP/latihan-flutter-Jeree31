8f25d386273276d5890b6d46cc7aace4009bf23d
Jeree31 <jeremyallegratohartono_2327250004@mhs.mdp.ac.id>
2024-11-23 11:55:55 +0700

    sign in dan sign up screen

--- wisata_candi/lib/screens/sign_in_screen.dart
+++ wisata_candi/lib/screens/sign_in_screen.dart
@@ -1,24 +1,22 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/sign_up_screen.dart';
 
-class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({super.key});
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
 
   @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
+  State<SignInScreen> createState() => _SignInScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
-  final TextEditingController _nameController = TextEditingController();
+class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   String _errorText = '';
-
   bool _obscurePassword = true;
 
-  // TODO: 1. Membuat fungsi _signUp
-  void _signUp() {
-    String name = _nameController.text.trim();
+  void _signIn() {
     String username = _usernameController.text.trim();
     String password = _passwordController.text.trim();
 
@@ -38,26 +36,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
       });
     }
 
-    print('*** Sign up berhasil!');
-    print('Nama: $name');
+    print('*** Sign in berhasil!');
     print('Nama Pengguna: $username');
     print('Password: $password');
-  }
-
-  // TODO: 2. Membuat fungsi dispose
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        title: const Text('Sign In'),
       ),
       body: Center(
         child: SingleChildScrollView(
@@ -65,56 +53,71 @@ class _SignUpScreenState extends State<SignUpScreen> {
             padding: const EdgeInsets.all(16),
             child: Form(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Nama',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
                   TextFormField(
                     controller: _usernameController,
                     decoration: const InputDecoration(
-                      labelText: 'Nama Pengguna',
+                      labelText: "Nama Pengguna",
                       border: OutlineInputBorder(),
                     ),
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
+                  const SizedBox(height: 20),
                   TextFormField(
                     controller: _passwordController,
                     decoration: InputDecoration(
-                      labelText: 'Kata Sandi',
+                      labelText: "Kata Sandi",
                       errorText: _errorText.isNotEmpty ? _errorText : null,
                       border: const OutlineInputBorder(),
                       suffixIcon: IconButton(
-                        icon: Icon(
-                          _obscurePassword
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                        ),
                         onPressed: () {
                           setState(() {
                             _obscurePassword = !_obscurePassword;
                           });
                         },
+                        icon: Icon(
+                          _obscurePassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
                       ),
                     ),
                     obscureText: _obscurePassword,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
+                  const SizedBox(height: 20),
                   ElevatedButton(
-                    onPressed: _signUp,
-                    child: const Text('Sign Up'),
+                    onPressed: _signIn,
+                    child: const Text('Sign In'),
+                  ),
+                  const SizedBox(height: 10),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Belum punya akun? ',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.deepPurple,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Daftar di sini.',
+                          style: const TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                            fontSize: 16,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignUpScreen(),
+                                ),
+                              );
+                            },
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
